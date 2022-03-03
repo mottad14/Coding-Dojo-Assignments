5f96cc80ad5d564effb2cd1d5bbde8755c439f5f
Danny M <93226206+mottad14@users.noreply.github.com>
2022-03-02 22:44:49 -0600

    Add files via upload

---
+++ ButtonClicker Project/script.js
@@ -0,0 +1,10 @@
+function loggedIn(element){
+    console.log("element clicked", element);
+    element.innerText = "Logout";
+}
+
+
+function removeButton(element){
+    console.log("element clicked", element);
+    element.remove();
+}
