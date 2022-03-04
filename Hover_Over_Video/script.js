c377a8e9f231be8ee914beed1ace609846edc1e4
Danny M <93226206+mottad14@users.noreply.github.com>
2022-03-03 23:38:41 -0600

    Add files via upload

---
+++ Hover_Over_Video/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function playVid(element){
+    console.log("You're on me")
+    element.play() 
+}
+
+function pauseVid(element){
+    console.log("You're leaving")
+    element.pause()
+}
